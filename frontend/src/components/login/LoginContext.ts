156ccb07bb7e3017d8a5b99e741ba9d758bb2b6a
Maximilian Diek <s87999@bht-berlin.de>
2023-11-29 16:08:56 +0100

    Check if JWT is expired on startup

--- frontend/src/components/login/LoginContext.ts
+++ frontend/src/components/login/LoginContext.ts
@@ -16,8 +16,15 @@ export function getLoginInfo(): LoginInfo | null {
         return null;
 
     const payload: JwtPayload & { role: "u" | "m" | "a" } = jwtDecode(jwt);
+    const exp: number = Number(payload.exp ?? 0);
     const userID: string | undefined = payload.sub;
     const role: "a" | "u" | "m" = payload.role;
+
+    if (Date.now() >= exp * 1000) {
+        removeJWT();
+        alert("Session expired\nYou need to log in again");
+        return null;
+    }
 
     if (!userID || !role)
         return null;
