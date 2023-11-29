2d12c1b9c3aac8a3ef30e26eed2ac06809865eab
Dominik H <s88126@bht-berlin.de>
2023-11-29 17:04:25 +0100

    Feature 76 https

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Express } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import loginRouter from "./endpoints/login/LoginRouter";
@@ -8,12 +8,25 @@ import postRouter from "./endpoints/post/PostRoute";
 import { threadPageRouter } from "./endpoints/threadpage/ThreadPageRoute";
 import threadRouter from "./endpoints/thread/ThreadRoute";
 
-const app: any = express();
-const port: number = 3001;
+const app: Express = express();
+const port: number = 443;
+
+app.use("*", cors({
+    origin: [
+        "https://127.0.0.1:3000",
+        "https://localhost:3000"
+    ]
+}));
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header("Access-Control-Expose-Headers", "Authorization");
+    next();
+});
 
 app.use("*", express.json());
 app.use(bodyParser.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+
 app.use("/api/login", loginRouter);
 app.use("/api/user", userRouter)
 app.use("/api/post", postRouter)
--- backend/src/testIndex.ts
+++ backend/src/testIndex.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use("*", express.json());
 app.use(bodyParser.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "http://127.0.0.1:3000" }));
 app.use("/api/login", loginRouter);
 app.use("/api/user", userRouter)
 app.use("/api/post", postRouter)
