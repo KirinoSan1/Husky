badae894cfc792e3e78486e6ad47318dfd573ee8
Can Pala <s88011@bht-berlin.de>
2023-12-02 20:36:04 +0100

    Feature 91 implement subforum entity

---
+++ backend/src/endpoints/subforum/SubForumModel.ts
@@ -0,0 +1,18 @@
+import { Model, model, Schema, Types } from "mongoose";
+
+export interface ISubForum {
+    name: string;
+    description: string;
+    threads: Types.ObjectId[];
+}
+
+type subForumOverride = { threads: Types.DocumentArray<Types.ObjectId> };
+type subForumModel = Model<ISubForum, {}, subForumOverride>;
+
+const subforumSchema = new Schema<ISubForum, subForumModel>({
+    name: { type: String, required: true },
+    description: { type: String },
+    threads: { type: [{ type: Schema.Types.ObjectId, ref: "Thread", required: true }], required: true}
+});
+
+export const SubForum = model<ISubForum, subForumModel>("SubForum", subforumSchema);
