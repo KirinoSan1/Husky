f6eba1500179f0fbef0d995d78ab4c62ee0d36b6
Mohammad Hammad <s88475@bht-berlin.de>
2023-12-16 22:44:43 +0100

    Feature 115 bug fixing search bar

--- frontend/src/components/thread/ThreadSearch.tsx
+++ frontend/src/components/thread/ThreadSearch.tsx
@@ -1,5 +1,5 @@
-import { useContext, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { ThreadResource } from "../../types/Resources";
 import { searchThreadsByTitle } from "../../api/api";
 import LoadingIndicator from "../util/LoadingIndicator";
@@ -13,21 +13,42 @@ export default function ThreadSearch() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
     const navigate = useNavigate();
+    const location = useLocation();
 
     const handleUpdate = (e: React.ChangeEvent<HTMLInputElement>) => { setTitle(e.target.value); };
+    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            handleSearch();
+        }
+    };
     const handleSearch = async () => {
         setLoading(true);
         setThreads(null);
         setError("");
         try {
-            if (title.length < 2)
+            if (title.length < 2) {
                 throw new Error("query must contain at least 2 characters");
+            }
+
             setThreads(await searchThreadsByTitle(title));
         } catch (error) {
             setError(String(error));
         }
         setLoading(false);
     };
+
+
+    useEffect(() => {
+        async function search() {
+            if (location.state) {
+                setThreads(await searchThreadsByTitle(location.state))
+            } else {
+                return;
+            }
+        }
+        search()
+    }, [])
+
 
     let count = 0;
     return (
@@ -37,7 +58,7 @@ export default function ThreadSearch() {
                 <div className="threadsearch-horizontal-line"></div>
             </>}
             <p id="threadsearch-div-p0">Search for threads by title:</p>
-            <input id="threadsearch-div-input" className="form-control" type="search" placeholder="Title..." onChange={handleUpdate} value={title}></input>
+            <input id="threadsearch-div-input" className="form-control" type="search" placeholder="Title..." onChange={handleUpdate} value={title} onKeyPress={handleKeyPress}></input>
             <Button id="threadsearch-div-button2" onClick={handleSearch}>Search</Button>
             {loading && <LoadingIndicator />}
             {(error || threads) && <div className="threadsearch-horizontal-line"></div>}
